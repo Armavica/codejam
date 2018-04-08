52cde7f9c986bae61babfaa24c0306e44292025a
Virgile Andreani <virgile.andreani@anbuco.fr>
2018-04-08 18:12:50 +0200

    B-197

--- 2018/qualif/b/bm.py
+++ 2018/qualif/b/bm.py
@@ -1,3 +1,4 @@
-for c in range(int(input())):
- input();*l,=map(int,input().split());l[::2],l[1::2]=map(sorted,(l[::2],l[1::2]))
+i=input
+for c in range(int(i())):
+ i();*l,=map(int,i().split());l[::2],l[1::2]=map(sorted,(l[::2],l[1::2]))
  print('Case #%d:'%-~c,['OK',*(i for i in range(len(l)-1)if l[i]>l[i+1])][l!=sorted(l)])

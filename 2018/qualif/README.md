e8a162b8d15a02bfb1c1ba582ed65323b1f919e7
Virgile Andreani <virgile.andreani@anbuco.fr>
2018-04-09 09:25:25 +0200

    A-250

--- 2018/qualif/README.md
+++ 2018/qualif/README.md
@@ -5,5 +5,5 @@
 3. [Go, Gopher!](https://codejam.withgoogle.com/2018/challenges/00000000000000cb/dashboard/0000000000007a30): Python3, 135 bytes
 4. [Cubic UFO](https://codejam.withgoogle.com/2018/challenges/00000000000000cb/dashboard/00000000000079cc): Python3, 185 bytes
 
-Total: 776 bytes
+Total: 767 bytes
 
--- 2018/qualif/a/am.py
+++ 2018/qualif/a/am.py
@@ -1,11 +1,6 @@
-def e(s):
- d=x=1
- for c in s:
-  if c<'D':x*=2
-  else:d+=x
- return~-d
+e=lambda s,p:0 if s<'B'else(s>'D')*p+e(s[1:],p+(s<'D')*p)
 for c in range(int(input())):
  v,s=input().split();n=v=int(v);y=v>=s.count('S')
  if y:
-  while e(s)>v:x=s.rfind('CS');s=s[:x]+'SC'+s[x+2:];n+=1
+  while e(s,1)>v:x=s.rfind('CS');s=s[:x]+'SC'+s[x+2:];n+=1
  print('Case #%d:'%-~c,['IMPOSSIBLE',n-v][y])
